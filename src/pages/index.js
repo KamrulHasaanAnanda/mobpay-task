57ba75a5ca86c3890c0603ea0864a043e4c3282f
KamnrulHassanAnanda <anaada2015@gmail.com>
2023-12-24 00:39:17 +0600

    currency modal added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,24 @@
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
-
+import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 export default function Home() {
+  const [open, setopen] = useState(false);
   return (
     <main className={`flex flex-col h-screen bg-black w-full `}>
       <div
@@ -22,9 +39,12 @@ export default function Home() {
             <h5 className="text-Content-Light-main text-base font-normal leading-normal text-white">
               Currency Denominations
             </h5>
-            <Button style={{
-              padding:5
-            }} className="rounded-lg text-base border-none bg-green-500 hover:opacity-75 hover:bg-green-600">
+            <Button
+              style={{
+                padding: 5,
+              }}
+              className="rounded-lg text-base border-none bg-green-500 hover:opacity-75 hover:bg-green-600"
+            >
               Add type
             </Button>
           </div>
@@ -114,6 +134,60 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      <Dialog open={open} onOpenChange={setopen}>
+        <DialogTrigger asChild>
+          <Button>Edit Profile</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[325px] w-11/12 bg-white border-none rounded">
+          <DialogHeader>
+            <DialogTitle>ADD CURRENCY</DialogTitle>
+          </DialogHeader>
+          <div className="grid py-4">
+          <div className="flex flex-col gap-2 mt-2 mb-4">
+              <Select>
+                <SelectTrigger className="w-full border border-gray-400 z-50">
+                  <SelectValue placeholder="Add denominations" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="light">Light</SelectItem>
+                  <SelectItem value="dark">Dark</SelectItem>
+                  <SelectItem value="system">System</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h4 htmlFor="name" className="font-semibold">
+                Currency Name
+              </h4>
+              <Input
+                id="c_name"
+                value="Dollar"
+                className="border border-gray-400"
+              />
+            </div>
+            <div className="flex flex-col gap-2 mt-2">
+              <h4 htmlFor="name" className="font-semibold">
+                Sign
+              </h4>
+              <Input
+                id="c_name"
+                value="Dollar"
+                className="border border-gray-400"
+              />
+            </div>
+        
+          </div>
+
+          <Button
+            type="submit"
+            className="w-full bg-black text-white hover:bg-slate-600"
+            varient
+          >
+            Save changes
+          </Button>
+        </DialogContent>
+      </Dialog>
     </main>
   );
 }
